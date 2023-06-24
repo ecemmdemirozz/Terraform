7dc2306cbfa986ccac31b398a3d15cc69b62982e
Ecem Demiroz <126841102+ecemmdemirozz@users.noreply.github.com>
2023-06-24 16:44:49 -0500

    Update providers.tf

--- ec2/providers.tf
+++ ec2/providers.tf
@@ -9,7 +9,7 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
-  shared_credentials_files = [""]
+  shared_credentials_files = ["/Users/fatihellek/.aws/credentials"]
   profile = "default"
   
 }
